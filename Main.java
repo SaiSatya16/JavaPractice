d2087d5e5db390a5dd8f37a1364b58eedd94b741
SaiSatya16 <103992082+SaiSatya16@users.noreply.github.com>
2023-07-02 14:36:00 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,30 @@
+class Car{
+  private int wheels;//instance variables
+  private int seats;
+  private string name;
+  public Car(int w, int s, string n){ //constructor
+    wheels = w;
+    Seats = s;
+    name = n;
+  }
+  public void breake(){
+    //method
+  }
+  public void start(){
+    //method
+  }
+  
+}
+
+
+
+
+class Main {
+  public static void main(String[] args) {
+
+    Car c1 = new Car(4,4,"car1");
+    Car c2 = new Car(5,3,"car2");
+    
+
+  }
+}
