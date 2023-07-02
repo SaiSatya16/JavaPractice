dd1e726974ac75fcd066309f93d782b4e951b275
SaiSatya16 <103992082+SaiSatya16@users.noreply.github.com>
2023-07-02 17:51:58 +0000

    constructors

--- Main.java
+++ Main.java
@@ -11,6 +11,11 @@ class Car{
     this(w,s); // calling the constructor from another constructior
     name = n;
   }
+  public Car(Car c){ //Copy Constructor
+    this.wheels = c.wheels;
+    this.name = c.name;
+    this.seats = c.seats;
+  }
   public void breake(){
     //method
     System.out.println(wheels);
@@ -29,7 +34,9 @@ class Main {
 
     Car c1 = new Car(4,4,"car1"); //object creation
     Car c2 = new Car(5,3,"car2");
-    System.out.println(c1.wheels);
+    Car c3 = new Car(c1); //copying values from c1
+    System.out.println("c1 Wheels: "+ c1.wheels);
+    System.out.println("C3 Wheels: "+ c3.wheels);
 
   }
 }
