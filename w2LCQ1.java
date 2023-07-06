d668febd9861edb783e765b1cafa5821ddf52f33
SaiSatya16 <103992082+SaiSatya16@users.noreply.github.com>
2023-07-06 15:53:09 +0000

    w2LCQ1

---
+++ w2LCQ1.java
@@ -0,0 +1,64 @@
+// Write the java program that creates 3 objects of students and prints the name of student with highest total. Implement the code as follows:
+// Define class Student with following members:
+// • String name, double [ marks (array length is 3) as instance variables which represents the name and marks of student
+// • Constructor to initialize instance variables
+// • Accessor method getName () that returns the name of student
+// • Method findTotal ( ) that returns the total marks of the student
+// Define class Test that has the following members:
+// • Method getMax () which takes Student [J as parameter and returns the name of the student with highest total.
+// • main () method, that creates 3 instances of Student, stores them in an array and invokes the method getMax () by passing that Student []
+
+import java.util.*;
+
+class Student{
+  private String name;
+  private double[] marks;
+  public Student(String name, double[] marks) {
+      this.name = name;
+      this.marks = marks;
+  }
+
+  public String getname(){
+    return name;
+  }
+
+  public double[] getmarks(){
+    return marks;
+  }
+  public double findTotal(){
+    double total =0;
+    for(double i: this.marks){
+      total = total + i;
+      
+    }
+    return total;
+  }
+  
+  
+}
+
+class Main {
+  public static String getmax(Student[] student){
+    double max_marks = 0.0;
+    String max_student = "";
+    for(Student i: student){
+      if(max_marks < i.findTotal()){
+        max_marks = i.findTotal();
+        max_student = i.getname();
+      }
+     
+    }
+    return max_student;
+  }
+  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    Student[] arr = new Student[3];
+    for(int i =0; i<3; i++){
+      String name = sc.next();
+      double m[] = {sc.nextDouble(),sc.nextDouble(),sc.nextDouble()};
+      arr[i] = new Student(name,m);
+    }
+    System.out.println(getmax(arr));
+  }
+}
